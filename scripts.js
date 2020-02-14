7e6f6d0503f8d5862f3cd1d0ca5e5cd6813945ed
David Goligorsky <d.goligorsky@gmail.com>
2020-02-14 08:37:22 -0500

    set up barba and gsap

---
+++ scripts.js
@@ -0,0 +1,41 @@
+barba.init({
+  debug: true,
+  transitions: [
+    {
+      name: "next",
+      leave({ current, next, trigger }) {
+        return new Promise(resolve => {
+          const timeline = gsap.timeline({
+            onComplete() {
+              resolve();
+            }
+          });
+
+          const navigation = current.container.querySelectorAll(
+            "header, a.next, a.previous"
+          );
+
+          timeline.to(navigation, { opacity: 0 });
+        });
+      },
+      enter({ current, next, trigger }) {
+        return new Promise(resolve => {
+          const timeline = gsap.timeline({
+            onComplete() {
+              resolve();
+            }
+          });
+
+          const navigation = next.container.querySelectorAll(
+            "header, a.next, a.previous"
+          );
+
+          timeline
+            .set(navigation, { opacity: 0 })
+            .to(navigation, { opacity: 1 });
+        });
+      }
+    }
+  ],
+  views: []
+});
